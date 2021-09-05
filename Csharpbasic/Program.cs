86a906a0f82df5fb09367d21fd4b7033828b2a8b
Shubham Mule <shubhambmule@gmail.com>
2021-09-05 12:15:27 +0530

    flip coin

---
+++ Csharpbasic/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Csharpbasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int coin;
+          
+            Random rng = new Random();
+            coin = rng.Next(0, 2);
+            if (coin==0)
+            {
+                Console.Write("Tails");
+            }
+            else
+            {
+                Console.Write("Heads");
+            }
+        }
+    }
+}
