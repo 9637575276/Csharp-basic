2b4a79d09d56cacc3cdf7c02abd05603729248f6
Shubham Mule <shubhambmule@gmail.com>
2021-10-11 11:06:14 +0530

    Checkind alphabets vowels or consonants

--- Csharpbasic/Program.cs
+++ Csharpbasic/Program.cs
@@ -4,21 +4,20 @@ namespace Csharpbasic
 {
     class Program
     {
-        public static void swap(int num1, int num2)
-        {
-            int num3 = 0;
-            Console.WriteLine($"Numbers before swapping are num1= {num1}and num2={num2}");
-            num3 = num1;
-            num1 = num2;
-            num2 = num1;
-            Console.WriteLine($"Numbers after swapping are num1={num1}and num2={num2}");
-        } 
-
+       
         static void Main(string[] args)
         {
 
-            Program.swap(5,7);
-
+            string alphabet;
+            alphabet=Console.ReadLine();
+            if(alphabet=="a" || alphabet=="e" || alphabet=="i"   ||alphabet=="o" ||alphabet=="u")
+            {
+                Console.WriteLine($"{alphabet} is a vowel");
+            }
+            else
+            {
+                Console.WriteLine($"{alphabet} is a consonant");
+            }
         }
 
     }
